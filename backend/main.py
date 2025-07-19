bfba6e48f3363e59e274b4ae6ebdcf99b034c433
Jatin Singh <153707623+jatin019@users.noreply.github.com>
2025-07-20 04:53:37 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI
-from backend.routes.conversation import router as conversation_router
+from routes.conversation import router as conversation_router
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
